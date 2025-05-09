b32ef1cc83883b8e1b75218ea441a571b12540f4
SonaniAkshit <sonaniakshit684@gmail.com>
2025-05-09 11:24:24 +0530

    commit

--- StarPatterns/src/Pattern1.java
+++ StarPatterns/src/Pattern1.java
@@ -1,12 +1,12 @@
-/*
-     *  *  *  *  *
-     *  *  *  *  *
-     *  *  *  *  *
-     *  *  *  *  *
-     *  *  *  *  *
- */
-
 import java.util.Scanner;
+
+/*
+        *  *  *  *  *
+        *  *  *  *  *
+        *  *  *  *  *
+        *  *  *  *  *
+        *  *  *  *  *
+*/
 public class Pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
